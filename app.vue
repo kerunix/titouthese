e8cc89240086b9e1af0e8bbfde277a459468922b
kerunix <pierre@kissmy.co>
2023-09-04 20:44:49 +0200

    maybe done maybe not

---
+++ app.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+</script>
+
+<template>
+  <ClientOnly>
+    <Scene />
+  </ClientOnly>
+</template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+
+#__nuxt {
+  height: 100%;
+  width: 100%;
+}
+</style>

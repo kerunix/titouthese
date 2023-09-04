a3f66e4afe1149cc07ac9375590a0e5b7febb960
kerunix <pierre@kissmy.co>
2023-09-04 20:58:10 +0200

    maybe working now ?

--- app.vue
+++ app.vue
@@ -1,24 +1,3 @@
-<script setup lang="ts">
-import { TresCanvas } from '@tresjs/core'
-</script>
-
 <template>
-  <ClientOnly>
-    <Scene />
-  </ClientOnly>
+  <NuxtPage />
 </template>
-
-<style>
-html,
-body {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  width: 100%;
-}
-
-#__nuxt {
-  height: 100%;
-  width: 100%;
-}
-</style>

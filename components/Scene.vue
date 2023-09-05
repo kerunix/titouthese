f771f428cdc3c4404ec54e9c8d3b14d3d6377d06
kerunix <pierre@kissmy.co>
2023-09-05 10:59:19 +0200

    one more try

--- components/Scene.vue
+++ components/Scene.vue
@@ -10,7 +10,6 @@ onMounted(() => {
 
     graph(graphWrapper.value).graphData(data)
   }
-
 })
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { TresCanvas } from '@tresjs/core'
 </script>
 
 <template>
+<div>
   <ClientOnly>
     <Scene />
   </ClientOnly>
+</div>
 </template>
 
 <style>
